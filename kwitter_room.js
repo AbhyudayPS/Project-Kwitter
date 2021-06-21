afee10ef076a58071e932b354092d30979d76df0
AbhyudayPS <74713991+AbhyudayPS@users.noreply.github.com>
2021-06-21 20:24:13 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+    apiKey: "AIzaSyCfp4kTu1IbAPwhv4JBCkCz2_oHkuDjQh8",
+    authDomain: "class-test-bdf82.firebaseapp.com",
+    databaseURL: "https://class-test-bdf82-default-rtdb.firebaseio.com",
+    projectId: "class-test-bdf82",
+    storageBucket: "class-test-bdf82.appspot.com",
+    messagingSenderId: "456963777690",
+    appId: "1:456963777690:web:afd37fcabb50f20a86eb23"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
